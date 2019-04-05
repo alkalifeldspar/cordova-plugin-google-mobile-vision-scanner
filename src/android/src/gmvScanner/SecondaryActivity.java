d7b4287add6cb887bc4314a3df0e183bcec286cf
Tristan <tristan@costar.ca>
2019-04-05 13:35:17 -0600

    Code cleanup

--- src/android/src/gmvScanner/SecondaryActivity.java
+++ src/android/src/gmvScanner/SecondaryActivity.java
@@ -1,21 +1,8 @@
-/*
- * Copyright (C) The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package tl.cordova.google.mobile.vision.scanner;
 
+// ----------------------------------------------------------------------------
+// |  Android Imports
+// ----------------------------------------------------------------------------
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
@@ -24,102 +11,90 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
+// ----------------------------------------------------------------------------
+// |  Google Imports
+// ----------------------------------------------------------------------------
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
 
-/**
- * Main activity demonstrating how to pass extra parameters to an activity that
- * reads barcodes.
- */
 public class SecondaryActivity extends Activity implements View.OnClickListener {
+  // ----------------------------------------------------------------------------
+  // | Public Properties
+  // ----------------------------------------------------------------------------
+  public static final String BarcodeObject = "Barcode";
 
-    // use a compound button so either checkbox or switch widgets work.
-    private CompoundButton autoFocus;
-    private CompoundButton useFlash;
-    private TextView statusMessage;
-    private TextView barcodeValue;
-    public static final String BarcodeObject = "Barcode";
+  // ----------------------------------------------------------------------------
+  // | Protected Properties
+  // ----------------------------------------------------------------------------
 
-    private static final int RC_BARCODE_CAPTURE = 9001;
-    private static final String TAG = "BarcodeMain";
+  // ----------------------------------------------------------------------------
+  // | Private Properties
+  // ----------------------------------------------------------------------------
+  private static final int    RC_BARCODE_CAPTURE = 9001         ;
+  private static final String TAG                = "BarcodeMain";
+  
+  // private CompoundButton autoFocus    ;
+  // private CompoundButton useFlash     ;
+  // private TextView       statusMessage;
+  // private TextView       barcodeValue ;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(getResources().getIdentifier("activity_gmv_barcode_scanner", "layout", getPackageName()));
+  // ----------------------------------------------------------------------------
+  // |  Public Functions
+  // ---------------------------------------------------------------------------- 
+  @Override
+  public void onClick(View p_View) {
+    if (p_View.getId() == getResources().getIdentifier("read_barcode", "id", getPackageName())) {
+      // launch barcode activity.
+      Intent intent = new Intent(this, BarcodeCaptureActivity.class);
 
-        findViewById(getResources().getIdentifier("read_barcode", "id", getPackageName())).setOnClickListener(this);
-
-        Intent intent = new Intent(this, BarcodeCaptureActivity.class);
-
-        intent.putExtra("DetectionTypes", getIntent().getIntExtra("DetectionTypes", 1234));
-        intent.putExtra("ViewFinderWidth", getIntent().getDoubleExtra("DetectionTypes", .5));
-        intent.putExtra("ViewFinderHeight", getIntent().getDoubleExtra("DetectionTypes", .7));
-
-        startActivityForResult(intent, RC_BARCODE_CAPTURE);
-
+      startActivityForResult(intent, RC_BARCODE_CAPTURE);
     }
+  }
 
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == getResources().getIdentifier("read_barcode", "id", getPackageName())) {
-            // launch barcode activity.
-            Intent intent = new Intent(this, BarcodeCaptureActivity.class);
+  // ----------------------------------------------------------------------------
+  // |  Protected Functions
+  // ----------------------------------------------------------------------------
+  @Override
+  protected void onCreate(Bundle p_SavedInstanceState) {
+    super.onCreate(p_SavedInstanceState);
+    setContentView(getResources().getIdentifier("activity_gmv_barcode_scanner", "layout", getPackageName()));
 
-            startActivityForResult(intent, RC_BARCODE_CAPTURE);
+    findViewById(getResources().getIdentifier("read_barcode", "id", getPackageName())).setOnClickListener(this);
+
+    Intent intent = new Intent(this, BarcodeCaptureActivity.class);
+
+    intent.putExtra("DetectionTypes", getIntent().getIntExtra("DetectionTypes", 1234));
+    intent.putExtra("ViewFinderWidth", getIntent().getDoubleExtra("DetectionTypes", .5));
+    intent.putExtra("ViewFinderHeight", getIntent().getDoubleExtra("DetectionTypes", .7));
+
+    startActivityForResult(intent, RC_BARCODE_CAPTURE);
+  }
+
+  @Override
+  protected void onActivityResult(int p_RequestCode, int p_ResultCode, Intent p_Data) {
+    Log.d(TAG, "Activity exited");
+    if (p_RequestCode == RC_BARCODE_CAPTURE) {
+      Intent d = new Intent();
+      if (p_ResultCode == CommonStatusCodes.SUCCESS) {
+        if (p_Data != null) {
+          Barcode barcode = p_Data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+          d.putExtra(BarcodeObject, barcode);
+          setResult(CommonStatusCodes.SUCCESS, p_Data);
+        } else {
+          d.putExtra("err", "USER_CANCELLED");
+          setResult(CommonStatusCodes.ERROR, d);
         }
-
+      } else {
+        d.putExtra("err", "There was an error with the barcode reader.");
+        setResult(CommonStatusCodes.ERROR, d);
+      }
+      finish();
+    } else {
+      super.onActivityResult(p_RequestCode, p_ResultCode, p_Data);
     }
+  }
 
-    /**
-     * Called when an activity you launched exits, giving you the requestCode
-     * you started it with, the resultCode it returned, and any additional
-     * data from it.  The <var>resultCode</var> will be
-     * {@link #RESULT_CANCELED} if the activity explicitly returned that,
-     * didn't return any result, or crashed during its operation.
-     * <p/>
-     * <p>You will receive this call immediately before onResume() when your
-     * activity is re-starting.
-     * <p/>
-     *
-     * @param requestCode The integer request code originally supplied to
-     *                    startActivityForResult(), allowing you to identify who this
-     *                    result came from.
-     * @param resultCode  The integer result code returned by the child activity
-     *                    through its setResult().
-     * @param data        An Intent, which can return result data to the caller
-     *                    (various data can be attached to Intent "extras").
-     * @see #startActivityForResult
-     * @see #createPendingResult
-     * @see #setResult(int)
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d(TAG, "Activity exited");
-        if (requestCode == RC_BARCODE_CAPTURE) {
-            Intent d = new Intent();
-            if (resultCode == CommonStatusCodes.SUCCESS) {
-                if (data != null) {
-                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
-                    d.putExtra(BarcodeObject, barcode);
-                    setResult(CommonStatusCodes.SUCCESS, data);
-                } else {
-                    d.putExtra("err", "USER_CANCELLED");
-                    setResult(CommonStatusCodes.ERROR, d);
-                }
-            } else {
-                d.putExtra("err", "There was an error with the barcode reader.");
-                setResult(CommonStatusCodes.ERROR, d);
-            }
-            finish();
-        }
-        else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
+  // ----------------------------------------------------------------------------
+  // |  Private Functions
+  // ----------------------------------------------------------------------------
 }
