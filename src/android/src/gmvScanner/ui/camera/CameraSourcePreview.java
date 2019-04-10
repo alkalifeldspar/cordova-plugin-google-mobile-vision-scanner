2ed8a731454ce4ede185d8e4fd5e3997ec6ccff1
Tristan <tristan@costar.ca>
2019-04-10 10:42:04 -0600

    no message

--- src/android/src/gmvScanner/ui/camera/CameraSourcePreview.java
+++ src/android/src/gmvScanner/ui/camera/CameraSourcePreview.java
@@ -208,7 +208,7 @@ public class CameraSourcePreview extends ViewGroup {
 
     
 
-    _VerticalLine.layout((layoutHeight-layoutWidth)/2, 10, layoutHeight-10, layoutHeight-10);
+    _VerticalLine.layout(((layoutHeight-layoutWidth)/2)*-1, 10, layoutHeight, layoutHeight-10);
 
     // _ViewFinderView.layout(
     //   layoutWidth / 2 - actualWidth / 2, layoutHeight / 2 - actualHeight / 2, 
